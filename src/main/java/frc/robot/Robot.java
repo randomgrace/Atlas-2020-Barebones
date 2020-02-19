af836e19300b85572ba74d41fe71a1dc7e8f3d57
Randy Groves <randomgrace@gmail.com>
2020-02-19 11:55:39 -0800

    Fix reference

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -36,7 +36,7 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
 	// Set Bling state to OFF
-	SmartDashboard.putString("robotInit/LED State: ", BlingState.BLING_COMMAND_OFF);
+	SmartDashboard.putString("robotInit/LED State: BLING_COMMAND_OFF");
 	Logger.notice("@robotInit: Requested BlingState.BLING_COMMAND_OFF");
     LED.getInstance().setBlingState(BlingState.BLING_COMMAND_OFF);
 
